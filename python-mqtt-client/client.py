6817cc0941c672dd9c1aadcd944d3ce511651dba
shakram02 <ahmedhamdyau@gmail.com>
2021-01-04 13:22:46 +0200

    use matplotlib

--- python-mqtt-client/client.py
+++ python-mqtt-client/client.py
@@ -1,5 +1,7 @@
 #!/bin/python3
-
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import datetime as dt
 import pandas as pd
 import json
 import paho.mqtt.client as mqtt
@@ -19,6 +21,15 @@ data_frame = pd.DataFrame(columns=[
                           )
 
 
+# Create figure for plotting
+fig, ax = plt.subplots(nrows=1, ncols=2)
+xs = []
+air_temp = []
+soil_temp = []
+air_humidity = []
+soil_humidity = []
+
+
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -31,10 +42,18 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(MQTT_TOPIC)
 
 
-def on_message(client, userdata: pd.DataFrame, msg):
+def on_message(client, userdata, msg):
     global data_frame
 
     data = json.loads(str(msg.payload, "UTF-8"))
+    print(data)
+    readings = data['data']
+    air_temp.append(readings["air"]["temp"])
+    soil_temp.append(readings["soil"]["temp"])
+
+    air_humidity.append(readings["air"]["humidity"])
+    soil_humidity.append(readings["soil"]["humidity"])
+
     updates = pd.json_normalize(data)
 
     data_frame = data_frame.append(updates)
@@ -42,11 +61,66 @@ def on_message(client, userdata: pd.DataFrame, msg):
     print(data_frame)
 
 
+def add_point(i, xs, air_temp, soil_temp, air_humidity, soil_humidity):
+    xs = xs[-20:]
+    air_temp = air_temp[-20:]
+    soil_temp = soil_temp[-20:]
+    air_humidity = air_humidity[-20:]
+    soil_humidity = soil_humidity[-20:]
+
+    # Draw x and y lists
+    ax[0].clear()
+    ax[0].plot(xs, air_temp)
+    ax[0].plot(xs, soil_temp)
+
+    ax[1].clear()
+    ax[1].plot(xs, air_humidity)
+    ax[1].plot(xs, soil_humidity)
+    plt.pause(1)
+
+
+# def animate(i, xs, air_temp, soil_temp, air_humidity, soil_humidity):
+#     # Read temperature (Celsius) from TMP102
+#     # temp_c = round(tmp102.read_temp(), 2)
+
+#     # Add x and y to lists
+#     # xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+#     # temps.append(temp_c)
+
+#     # Limit x and y lists to 20 items
+#     xs = xs[-20:]
+#     air_temp = air_temp[-20:]
+#     soil_temp = soil_temp[-20:]
+#     air_humidity = air_humidity[-20:]
+#     soil_humidity = soil_humidity[-20:]
+
+#     # Draw x and y lists
+#     ax[0].clear()
+#     ax[0].plot(xs, air_temp)
+#     ax[0].plot(xs, soil_temp)
+
+#     ax[1].clear()
+#     ax[1].plot(xs, air_humidity)
+#     ax[1].plot(xs, soil_humidity)
+
+#     # Format plot
+#     plt.xticks(rotation=45, ha='right')
+#     # plt.subplots_adjust(bottom=0.30)
+#     # plt.title('TMP102 Temperature over Time')
+#     # plt.ylabel('Temperature (deg C)')
+
+
 def main():
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(MQTT_BROKER, port=MQTT_PORT)  # connect to MQTT broke
+
+    # Set up plot to call animate() function periodically
+    ani = animation.FuncAnimation(
+        fig, add_point, fargs=(xs, air_temp, soil_temp, air_humidity, soil_humidity), interval=1000)
+
+    plt.show()
 
     client.loop_forever()
 
