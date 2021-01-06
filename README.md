e5fc3de100c343bb0c8e4fd74706e9908928e269
shakram02 <ahmedhamdyau@gmail.com>
2021-01-06 14:35:52 +0200

    cleanup + README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plant Watcher - Client Application
+
+A python client that utilizes MQTT protocol to graph plant data coming from sensor nodes through MQTT server.
+
+### System Components
+
+- MQTT Client
+- Plotter
+- JSON db storage
--- python-mqtt-client/client.py
+++ python-mqtt-client/client.py
@@ -1,25 +1,19 @@
 #!/bin/python3
+# https://learn.sparkfun.com/tutorials/graph-sensor-data-with-python-and-matplotlib/update-a-graph-in-real-time
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import datetime as dt
-import pandas as pd
 import json
-import paho.mqtt.client as mqtt
 import os
+import paho.mqtt.client as mqtt
+import threading
+from tinydb import TinyDB
+from dotenv import load_dotenv
 
-
-MQTT_BROKER = "174.138.103.162"
-MQTT_TOPIC = "/hello_world"
-MQTT_PORT = 3882
-data_frame = pd.DataFrame(columns=[
-                          "timestamp",
-                          "uuid",
-                          "data.air.humidity",
-                          "data.air.temp",
-                          "data.soil.temp",
-                          "data.soil.humidity"]
-                          )
-
+load_dotenv()
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_TOPIC = os.getenv("MQTT_TOPIC")
+MQTT_PORT = int(os.getenv("MQTT_PORT"))
 
 # Create figure for plotting
 fig, ax = plt.subplots(nrows=1, ncols=2)
@@ -28,10 +22,11 @@ air_temp = []
 soil_temp = []
 air_humidity = []
 soil_humidity = []
+is_updated = False
+db_timestamp = dt.datetime.now().strftime('%d-%m-%y_%H:%M:%S')
 
-
-def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
+db = TinyDB(f'db_{db_timestamp}.json',
+            sort_keys=True, indent=4, separators=(',', ': '))
 
 
 def on_connect(client, userdata, flags, rc):
@@ -43,25 +38,34 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    global data_frame
+    global is_updated
+    global db
 
     data = json.loads(str(msg.payload, "UTF-8"))
     print(data)
     readings = data['data']
-    air_temp.append(readings["air"]["temp"])
-    soil_temp.append(readings["soil"]["temp"])
+    if readings["air"]["temp"]:
+        air_temp.append(readings["air"]["temp"])
 
-    air_humidity.append(readings["air"]["humidity"])
-    soil_humidity.append(readings["soil"]["humidity"])
+    if readings["soil"]["temp"]:
+        soil_temp.append(readings["soil"]["temp"])
 
-    updates = pd.json_normalize(data)
+    if readings["air"]["humidity"]:
+        air_humidity.append(readings["air"]["humidity"])
 
-    data_frame = data_frame.append(updates)
-    cls()
-    print(data_frame)
+    if readings["soil"]["humidity"]:
+        soil_humidity.append(readings["soil"]["humidity"])
+
+    xs.append(dt.datetime.now().strftime('%H:%M:%S'))
+    is_updated = True
+    db.insert(data)
 
 
 def add_point(i, xs, air_temp, soil_temp, air_humidity, soil_humidity):
+    global is_updated
+    if not is_updated:
+        return
+
     xs = xs[-20:]
     air_temp = air_temp[-20:]
     soil_temp = soil_temp[-20:]
@@ -72,42 +76,17 @@ def add_point(i, xs, air_temp, soil_temp, air_humidity, soil_humidity):
     ax[0].clear()
     ax[0].plot(xs, air_temp)
     ax[0].plot(xs, soil_temp)
+    ax[0].set_ylim([15, 25])
+    ax[0].set_xticklabels(xs, rotation=45, ha='right')
+    ax[0].set_title('Temperature')
 
     ax[1].clear()
-    ax[1].plot(xs, air_humidity)
+    ax[1].plot(xs, air_humidity,)
     ax[1].plot(xs, soil_humidity)
-    plt.pause(1)
-
-
-# def animate(i, xs, air_temp, soil_temp, air_humidity, soil_humidity):
-#     # Read temperature (Celsius) from TMP102
-#     # temp_c = round(tmp102.read_temp(), 2)
-
-#     # Add x and y to lists
-#     # xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-#     # temps.append(temp_c)
-
-#     # Limit x and y lists to 20 items
-#     xs = xs[-20:]
-#     air_temp = air_temp[-20:]
-#     soil_temp = soil_temp[-20:]
-#     air_humidity = air_humidity[-20:]
-#     soil_humidity = soil_humidity[-20:]
-
-#     # Draw x and y lists
-#     ax[0].clear()
-#     ax[0].plot(xs, air_temp)
-#     ax[0].plot(xs, soil_temp)
-
-#     ax[1].clear()
-#     ax[1].plot(xs, air_humidity)
-#     ax[1].plot(xs, soil_humidity)
-
-#     # Format plot
-#     plt.xticks(rotation=45, ha='right')
-#     # plt.subplots_adjust(bottom=0.30)
-#     # plt.title('TMP102 Temperature over Time')
-#     # plt.ylabel('Temperature (deg C)')
+    ax[1].set_ylim([50, 100])
+    ax[1].set_xticklabels(xs, rotation=45, ha='right')
+    ax[1].set_title('Humidity')
+    is_updated = False
 
 
 def main():
@@ -118,11 +97,11 @@ def main():
 
     # Set up plot to call animate() function periodically
     ani = animation.FuncAnimation(
-        fig, add_point, fargs=(xs, air_temp, soil_temp, air_humidity, soil_humidity), interval=1000)
+        fig, add_point, fargs=(xs, air_temp, soil_temp, air_humidity, soil_humidity), interval=1*3*1000)
 
+    th = threading.Thread(target=client.loop_forever, daemon=True)
+    th.start()
     plt.show()
-
-    client.loop_forever()
 
 
 if __name__ == "__main__":
